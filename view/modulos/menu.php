40d897c9ce4b8ee1cddf632f27c64e2ca6e6cec4
desarrollo <desarrollo@masoft.net>
2017-03-06 20:36:35 -0500

    vvvv

--- view/modulos/menu.php
+++ view/modulos/menu.php
@@ -110,21 +110,23 @@ $controladores=$_SESSION['controladores'];
       </ul>
      
      
-       <form class="navbar-form" role="search" action="<?php echo $helper->url("Documentos","Buscador");?>"  method="post" >
+       <form class="navbar-form" role="search" action="<?php echo $helper->url("Documentos","Buscador");?>"  method="post" class="col-lg-5"  >
   		
   		    <div class="row">
-		    <div class="col-xs-2 col-md-2">
+		    
+		    
+		    <div class="col-xs-12 col-md-2">
 		    <div class="form-group">
                                 <input type="text" class="form-control" id="criterio_busqueda" name="contenido_busqueda" value=""  placeholder="texto a buscar">
             </div>
             </div>
-            <div class="col-xs-2 col-md-2">
+            <div class="col-xs-12 col-md-2">
             <div class="form-group">
                                  
                                   <select name="criterio_busqueda" id="criterio_busqueda"  class="form-control" >
                                   <option value="0"  > --TODOS--</option>
-									<option value="1"  >Identificacion Cliente/Proveedor</option>
-									<option value="2"  >Nombre Cliente/Proveedor</option>
+									<option value="1"  >Identificacion Cliente</option>
+									<option value="2"  >Nombre Cliente</option>
 									<option value="3"  >Número Carpeta</option>
 									<option value="4"  >Número Credito</option>
 									
@@ -141,12 +143,11 @@ $controladores=$_SESSION['controladores'];
                                   
             </div>
 		    </div>
-		    <div class="col-xs-2 col-md-1" style="margin-left: 40px">
+		    <div class="col-xs-12 col-md-1" style="margin-left: 10px">
 		    <button type="submit"  name="btn_buscar" class="btn btn-default"><span class="glyphicon glyphicon-search" ></span></button>
             </div>
 			</div> 
-  		
-  		
+  			
   		
   		
 		
